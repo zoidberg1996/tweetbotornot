8b92a015c379f8110c11d7b4d34c932509e78735
zoidberg1996 <54036072+zoidberg1996@users.noreply.github.com>
2019-08-12 18:20:02 +0200

    Update tweetbotornot.R

--- R/tweetbotornot.R
+++ R/tweetbotornot.R
@@ -87,12 +87,16 @@ botornot.factor <- function(x, fast = FALSE) {
 
 #' @export
 botornot.character <- function(x, fast = FALSE) {
-  ## remove NA and duplicates
-  x <- x[!is.na(x) & !duplicated(x)]
-  ## get most recent 100 tweets
-  x <- rtweet::get_timelines(x, n = 100)
-  ## pass to next method
-  botornot(x, fast = fast)
+x <- x[!is.na(x) & !duplicated(x)]
+if (fast)
+{
+x <- rtweet::lookup_users(x)
+}
+else
+{
+x <- rtweet::get_timelines(x, n = 100)
+}
+botornot(x, fast = fast)
 }
 
 
